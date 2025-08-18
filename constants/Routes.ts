c5c0fef3af553423342c521737e008206635de21
Junatan  Marc Cadayona <ljohnolayvar32@gmail.com>
2025-08-18 21:07:44 +0800

    update online

--- constants/Routes.ts
+++ constants/Routes.ts
@@ -2,4 +2,5 @@ export const routes = {
   customers: `${process.env.EXPO_API_LINK}/customers`,
   items: `${process.env.EXPO_API_LINK}/items`,
   salesorder: `${process.env.EXPO_API_LINK}/sales-orders`,
+  salescreate: `${process.env.EXPO_API_LINK}/sales-order`,
 };
